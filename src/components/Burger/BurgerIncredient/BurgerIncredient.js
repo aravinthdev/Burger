282efd32f63de869403c964046ac9d9b064cf108
naveenkarthika <yuvannaveen65@gmail.com>
2020-12-17 08:58:58 +0000

    PropTypes added function change into class component

--- src/components/Burger/BurgerIncredient/BurgerIncredient.js
+++ src/components/Burger/BurgerIncredient/BurgerIncredient.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Classes from './BurgerIncredient.module.css';
+import PropTypes from "prop-types";
 
-const burgerIncredient = (props) => {
-    let ingcredient = null;
+class BurgerIncredient extends Component {
+    render() {
+        let ingcredient = null;
 
-    switch (props.type) {
-        case ('bread-bottom'):
-            ingcredient = <div className={Classes.BreadBottom}></div>;
-            break;
-        case ('bread-top'):
-            ingcredient = (
-                <div className={Classes.BreadTop}>
-                    <div className={Classes.Seed1}></div>
-                    <div className={Classes.Seed2}></div>
-                </div>
-            );
-            break;
-        case ('Meat'):
-            ingcredient = <div className={Classes.Meat}></div>;
-            break;
-        case ('Bacon'):
-            ingcredient = <div className={Classes.Bacon}></div>;
-            break;
-        case ('Salad'):
-            ingcredient = <div className={Classes.Salad}></div>;
-            break;
-        case ('Cheese'):
-            ingcredient = <div className={Classes.Cheese}></div>;
-            break;
-        default:
-            ingcredient = null;
-
-
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingcredient = <div className={Classes.BreadBottom}></div>;
+                break;
+            case ('bread-top'):
+                ingcredient = (
+                    <div className={Classes.BreadTop}>
+                        <div className={Classes.Seed1}></div>
+                        <div className={Classes.Seed2}></div>
+                    </div>
+                );
+                break;
+            case ('Meat'):
+                ingcredient = <div className={Classes.Meat}></div>;
+                break;
+            case ('Bacon'):
+                ingcredient = <div className={Classes.Bacon}></div>;
+                break;
+            case ('Salad'):
+                ingcredient = <div className={Classes.Salad}></div>;
+                break;
+            case ('Cheese'):
+                ingcredient = <div className={Classes.Cheese}></div>;
+                break;
+            default:
+                ingcredient = null;
+               
+        }
+        return ingcredient;
     }
 }
 
-export default burgerIncredient;
+BurgerIncredient.propTypes{
+    type:PropTypes.string.isRequired
+}
+export default BurgerIncredient;
