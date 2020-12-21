8ab18f9705464a3ceabe412c9f0ce92a77d28abc
naveenkarthika <yuvannaveen65@gmail.com>
2020-12-21 09:03:37 +0000

    Check the length of the ingredient & display the msg

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,19 @@ import classes from './Burger.module.css';
 import BurgerIncredient from './BurgerIncredient/BurgerIncredient';
 
 const Burger = (props) => {
-    const transformedIngredient = Object.keys(props.ingredients)
+    let transformedIngredient = Object.keys(props.ingredients)
     .map(igKey => {
         console.log(igKey)
         return[...Array(props.ingredients[igKey])]
         .map((_,i) => {
                 return <BurgerIncredient key={igKey + i} type={igKey}/>
             })
-        })
+        }).reduce((arr,el) => {
+            return arr.concat(el)
+        },[])
+        if(transformedIngredient.length === 0){
+            transformedIngredient = <p>Please start adding with ingredients</p>
+        }
     console.log(transformedIngredient)
     return(
         <div className={classes.Burger}>
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     state = {
         ingredients : {
-            Meat: 2,
-            Salad: 1,
-            Becon: 2,
-            Cheese: 2
+            Meat: 0,
+            Salad: 0,
+            Becon: 0,
+            Cheese: 0
         }
     }
     render() {
