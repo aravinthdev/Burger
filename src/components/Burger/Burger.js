af132a355eaaf48d41c2c7b2d6ca83e669849843
naveenkarthika <yuvannaveen65@gmail.com>
2020-12-21 07:50:23 +0000

    In Burger value controlled by state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import classes from './Burger.module.css';
 import BurgerIncredient from './BurgerIncredient/BurgerIncredient';
 
 const Burger = (props) => {
+    const transformedIngredient = Object.keys(props.ingredients)
+    .map(igKey => {
+        console.log(igKey)
+        return[...Array(props.ingredients[igKey])]
+        .map((_,i) => {
+                return <BurgerIncredient key={igKey + i} type={igKey}/>
+            })
+        })
+    console.log(transformedIngredient)
     return(
         <div className={classes.Burger}>
             <BurgerIncredient type="bread-top"/>
-            <BurgerIncredient type="Meat"/>
-            <BurgerIncredient type="Bacon"/>
-            <BurgerIncredient type="Salad"/>
-            <BurgerIncredient type="Cheese"/>
+            {transformedIngredient}
             <BurgerIncredient type="bread-bottom"/>
         </div>
     )
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Auxs from '../../hoc/Auxs';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients : {
+            Meat: 2,
+            Salad: 1,
+            Becon: 2,
+            Cheese: 2
+        }
+    }
     render() {
         return(
             <Auxs>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Burger Controls</div>
             </Auxs>
         )
