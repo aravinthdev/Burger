6ca7d5bc1c04bf1ffc4dbe2f7b0305c74fa547e0
naveenkarthika <yuvannaveen65@gmail.com>
2020-12-22 22:48:44 +0000

    Backdrop is added

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import classes from './Modal.module.css'
+import Auxs from '../../../hoc/Auxs';
+import Backdrop from '../Backdrop/Backdrop'
+import classes from './Modal.module.css';
 
 const modal = (props) => (
-    <div
-        className={classes.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}
-    >
-        {props.children}
-    </div>
+    <Auxs>
+        <Backdrop show={props.show}/>
+        <div
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}
+        >
+            {props.children}
+        </div>
+    </Auxs>
+
 )
 
 export default modal;
